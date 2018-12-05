ff2facce3cf8df077ed047e700a6d06109b1fccb
Grant Wilk <grant@remingtongraphics.net>
2018-12-04 21:06:37 -0600

    Added GNU GPL license headers in all .py files

--- DNOISE/optix.py
+++ DNOISE/optix.py
@@ -1,3 +1,21 @@
+"""
+This file is part of D-NOISE: AI-Acclerated Denoiser.
+
+D-NOISE: AI-Acclerated Denoiser is free software: you can redistribute
+it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+D-NOISE: AI-Acclerated Denoiser is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with D-NOISE: AI-Acclerated Denoiser.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 import bpy
 import os
 from mathutils import Vector
